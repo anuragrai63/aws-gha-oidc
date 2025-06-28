e651338aaac0aa6ef1ecef5ef0a97625f23322ae
Anurag Rai <anuragrai@Anurags-MacBook-Air.local>
2025-06-29 00:33:21 +0530

    eks

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -255,7 +255,7 @@ resource "aws_security_group" "my_sg" {
     from_port   = "22"
     to_port     = "22"
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
@@ -269,7 +269,7 @@ resource "aws_security_group" "my_sg" {
     from_port   = "80"
     to_port     = "80"
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
